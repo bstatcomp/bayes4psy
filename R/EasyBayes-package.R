e6debdedf01fdfb5e9818f97e646d06ede3ee6a8
demsarjure <demsarjure@gmail.com>
2018-05-14 13:54:20 +0200

    Init stan package skeleton

---
+++ R/EasyBayes-package.R
@@ -0,0 +1,17 @@
+#' The 'EasyBayes' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name EasyBayes-package
+#' @aliases EasyBayes
+#' @useDynLib EasyBayes, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version NA. http://mc-stan.org
+#' 
+NULL
